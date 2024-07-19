521fb4cb8390f9268f4ca435d9a32abf88347862
Aleksey S <94798542+Alsidryid@users.noreply.github.com>
2024-07-19 20:18:20 +0200

    add contacts

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import picture from "../../images/logo2.svg";
 import style from "./Footer.module.css";
-
+import FooterContacts from "./FooterContacts/FooterContats";
 const Footer = () => {
   return (
     <div className={style.footer}>
@@ -8,6 +8,7 @@ const Footer = () => {
         <a href="www.aid.com" className={style.logo}>
           <img className={style.image} src={picture} alt="phone" />
         </a>
+        <FooterContacts />
       </div>
     </div>
   );
