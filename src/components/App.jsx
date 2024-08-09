a3df5a69a92c09e7434f7ccedc2753a6f9cc3013
Aleksey S <94798542+Alsidryid@users.noreply.github.com>
2024-08-09 16:46:58 +0200

    add loader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
-import EoEApp from "./EoEApp/EoEApp";
+import React, { Suspense, lazy } from "react";
+import Loader from "./Loader/Loader";
+
+const EoEApp = lazy(() => import("./EoEApp/EoEApp"));
 
 const App = () => {
   return (
-    <>
+    <Suspense fallback={<Loader />}>
       <EoEApp />
-    </>
+    </Suspense>
   );
 };
-
 export default App;
